cede0d7ab580c850370c1fe5051d035647a42cf7
Alberto Alvarez Garcia <aag_uk@yahoo.com>
2019-10-28 06:15:05 -0400

    modal dialogs

---
+++ src/main/resources/static/scripts/modal_dialog.js
@@ -0,0 +1,26 @@
+var newUrl;
+var modalConfirm = function(class_name){
+  $(class_name).on("click", function(){
+	  newUrl = document.activeElement.value;
+    $("#mi-modal").modal('show');
+  });
+
+  $("#modal-btn-si").on("click", function(){
+    $("#mi-modal").modal('hide');
+    window.location = newUrl;
+    newUrl = "";
+  });
+  
+  $("#modal-btn-no").on("click", function(){
+    $("#mi-modal").modal('hide');
+    newUrl = "";
+  });
+};
+
+function setDialogClassName(name) {
+	return name;
+}
+
+function setupDeleteOrderConfirmDialog() {
+	modalConfirm(setDialogClassName(".delete_order_confirm_dialog"));
+}
